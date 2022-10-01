df1776abb463bcfefe37d2cfe7bbb0c53fda7ce8
zhjbbt <zhj_bbt@126.com>
2022-10-01 11:58:48 +0800

    Site updated: 2022-10-01 11:58:44

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","dee6e35def7f799039a709691b73fc85"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
